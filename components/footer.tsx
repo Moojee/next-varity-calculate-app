9ca180fd6f73022ea1b99bceb344d2c1e36d695c
moojee <sutitamoojee@gmail.com>
2025-10-05 16:21:15 +0700

    Create hompage done

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <>
+    <div>
+        <h1 className="text-center text-lg text-gray-800" >
+            created by Sutita DTI-SAU
+        </h1>
+        <h1 className="text-center text-lg text-gray-800">
+            Copyrignt ⓒ 2025 Southeast Asia University
+        </h1>
+    </div>
+      
+    </>
+  );
+}
